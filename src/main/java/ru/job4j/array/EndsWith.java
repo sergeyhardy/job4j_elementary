83b8cd983f9d0059a95ca79df5131216fb5a624a
sergeyhardy <sergeyhardy@gmail.com>
2021-11-11 23:06:05 +0900

    mod

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -4,7 +4,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            if (post[post.length - 1 - index] != word[word.length - 1 - index]) {
+            if (post[post.length - 1 - index] != word[word.length - 1 - index])  {
                 result = false;
                 break;
 
