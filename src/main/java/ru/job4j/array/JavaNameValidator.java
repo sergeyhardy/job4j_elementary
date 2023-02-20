5bb452c3e6ab11283843a069df8d0fa513ca889f
sergeyhardy <sergeyhardy@gmail.com>
2023-02-20 22:13:12 +0900

    String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,41 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean valid = false;
+        if (name.isEmpty()) {
+            return valid;
+        }
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (isSpecialSymbol(code)) {
+                valid = true;
+            } else if (isUpperLatinLetter(code)) {
+                valid = true;
+            } else if (isLowerLatinLetter(code)) {
+                valid = true;
+            } else if (Character.isDigit(code)) {
+                valid = true;
+            } else {
+                valid = false;
+                break;
+            }
+        }
+        if (Character.isUpperCase(name.charAt(0)) || Character.isDigit(name.charAt(0))) {
+            valid = false;
+        }
+        return valid;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
